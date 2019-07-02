293afbd8d4d5c0c46e4ac7394098237fd209d37f
Anusha0629 <anusha.sg.29@gmail.com>
2019-07-02 10:14:00 +0530

    third

--- src/test/java/org/base/Test1.java
+++ src/test/java/org/base/Test1.java
@@ -1,7 +1,10 @@
 package org.base;
 
+import org.testng.annotations.Test;
+
 public class Test1 {
-	public static void main(String[] args) {
+	@Test
+	public  void test1() {
 		System.out.println("hello java");
 	}
 
